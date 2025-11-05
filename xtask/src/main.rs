1c28b29389d8a0b3ffa2a1286230d24dbfaca234
Tom Stokes <tomstokes@radixengineering.com>
2025-11-05 13:02:03 -0700

    Build wasm and run wasm-bindgen
    cargo xtask run-wasm will now build for wasm32-unknown-unkonwn and run wasm-bindgen on the output
    
    Signed-off-by: Tom Stokes <tomstokes@radixengineering.com>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,7 @@
 mod run_wasm;
+
 use run_wasm::RunWasm;
+use std::path::Path;
 
 use clap::{ArgMatches, Command};
 use xshell::Shell;
@@ -16,6 +18,8 @@ fn main() {
         .subcommand(Command::new("run-wasm").about("Build and serve wasm version"));
     let matches = cmd.get_matches();
     let shell = Shell::new().unwrap();
+    let workspace_root_path = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
+    shell.change_dir(workspace_root_path);
     match matches.subcommand() {
         Some(("run-wasm", sub_matches)) => RunWasm::run(&shell, sub_matches),
         _ => unreachable!(),
--- xtask/src/run_wasm.rs
+++ xtask/src/run_wasm.rs
@@ -7,7 +7,11 @@ pub(crate) struct RunWasm {}
 impl XtaskCommand for RunWasm {
     fn run(shell: &Shell, _matches: &ArgMatches) {
         // TODO: Return exit codes on error
-        xshell::cmd!(shell, "echo Run wasm is a work in progress")
+        // TODO: Support release and debug targets
+        xshell::cmd!(shell, "cargo build --target wasm32-unknown-unknown")
+            .run()
+            .unwrap();
+        xshell::cmd!(shell, "wasm-bindgen target/wasm32-unknown-unknown/debug/wgpucube.wasm --target web --out-dir target/generated --out-name wgpucube")
             .run()
             .unwrap();
     }
