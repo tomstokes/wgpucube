3def3dad8a5f71c4f0e0be9bffb2b4bef77fff84
Tom Stokes <tomstokes@radixengineering.com>
2025-11-05 11:39:17 -0700

    Add clap to xtask
    Add clap for argument parsing in the xtask script.
    
    Use the builder pattern instead of the derive pattern for slightly better compile times.
    
    clap is complete overkill for xtask, but I would like to make xtask user-friendly with clap's help messages, colors, and other amenities. I also anticipate using clap in the main wgpucube bin so using it here for consistency is a good idea.
    
    Signed-off-by: Tom Stokes <tomstokes@radixengineering.com>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,15 @@
+use clap::Command;
+
 fn main() {
-    println!("Hello, xtask!");
+    let cmd = clap::Command::new("xtask")
+        .bin_name("xtask")
+        .subcommand_required(true)
+        .subcommand(Command::new("run-wasm").about("Build and serve wasm version"));
+    let matches = cmd.get_matches();
+    match matches.subcommand() {
+        Some(("run-wasm", _)) => {
+            println!("run-wasm not implemented yet");
+        }
+        _ => unreachable!(),
+    }
 }
