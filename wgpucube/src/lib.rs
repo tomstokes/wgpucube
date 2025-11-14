aa1f4809e92b672e697cd84b08a6cf06623b66db
Tom Stokes <tomstokes@radixengineering.com>
2025-11-14 11:37:18 -0700

    Split main, lib, and app into separate files
    Refactor the app structure so main.rs and lib.rs can be separate files.
    
    Move the app code to app.rs and have both the bin and lib entry points import it.
    
    Signed-off-by: Tom Stokes <tomstokes@radixengineering.com>

---
+++ wgpucube/src/lib.rs
@@ -0,0 +1,24 @@
+#![cfg(target_os = "android")]
+
+mod app;
+mod cube;
+
+use app::{App, WgpuEvent};
+use winit::event_loop::EventLoop;
+use winit::platform::android::activity::AndroidApp;
+
+#[unsafe(no_mangle)]
+fn android_main(android_app: AndroidApp) {
+    use winit::platform::android::EventLoopBuilderExtAndroid;
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::Level::Trace.to_level_filter()),
+    );
+
+    let event_loop = EventLoop::<WgpuEvent>::with_user_event()
+        .with_android_app(android_app)
+        .build()
+        .unwrap();
+    let event_loop_proxy = event_loop.create_proxy();
+    let mut app = App::new(event_loop_proxy);
+    event_loop.run_app(&mut app).unwrap();
+}
